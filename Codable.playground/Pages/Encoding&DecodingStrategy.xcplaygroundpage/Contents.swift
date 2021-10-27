42dc4a00d2be3b07a59030a2d57c3610e59d6eb2
Marlon Ruiz <mruiz723@gmail.com>
2021-10-27 10:58:00 -0500

    Adding the Codable Playground.

---
+++ Codable.playground/Pages/Encoding&DecodingStrategy.xcplaygroundpage/Contents.swift
@@ -0,0 +1,67 @@
+//: [Encoded&DecodedManually](@previous)
+
+import Foundation
+
+let jsonString = """
+{
+    "abilities": [
+        {
+            "ability": {
+                "name": "overgrow",
+                "url": "https://pokeapi.co/api/v2/ability/65/"
+            },
+            "is_hidden": false,
+            "slot": 1
+        },
+        {
+            "ability": {
+                "name": "chlorophyll",
+                "url": "https://pokeapi.co/api/v2/ability/34/"
+            },
+            "is_hidden": true,
+            "slot": 3
+        }
+    ],
+    "base_experience": 64,
+    "forms": [
+        {
+            "name": "bulbasaur",
+            "url": "https://pokeapi.co/api/v2/pokemon-form/1/"
+        }
+    ]
+}
+"""
+
+struct Item: Codable {
+    let name: String
+    let url: String
+}
+
+struct Ability: Codable {
+    let ability: Item
+    let isHidden: Bool
+    let slot: Int
+}
+
+struct Pokemon: Codable {
+    let abilities: [Ability]
+    let forms: [Item]
+    let baseExperience: Int
+
+    enum CodingKeys: CodingKey {
+        case abilities
+        case forms
+        case baseExperience
+    }
+}
+
+let data = Data(jsonString.utf8)
+let decoder = JSONDecoder()
+decoder.keyDecodingStrategy = .convertFromSnakeCase
+let coordinate = try decoder.decode(Pokemon.self, from: data)
+
+let encoder = JSONEncoder()
+encoder.keyEncodingStrategy = .convertToSnakeCase
+let dataEncoded = try encoder.encode(coordinate)
+let jsonString2 = String(data: dataEncoded, encoding: .utf8)
+

7632f1a1ed60b210baef64629047adedf59b94d9
Jorge A. Benavides <jorge.benavides@wizeline.com>
2021-11-17 11:55:16 -0600

    Publishers and subscribers

---
+++ Combine.playground/Contents.swift
@@ -0,0 +1,116 @@
+import Foundation
+import Combine
+
+class Clock {
+
+    var seconds: Int = 0
+
+    let notificationCenter = NotificationCenter.default
+
+    func start() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] _ in
+            guard let clock = self else { return }
+            clock.seconds += 1
+            clock.notificationCenter.post(name: .tickingClock, object: clock)
+        })
+    }
+}
+
+let clock = Clock()
+
+extension Notification.Name {
+    static let tickingClock = Notification.Name(rawValue: "Clock.notification.ticking")
+}
+
+let notificationPublisher = NotificationCenter.Publisher(center: clock.notificationCenter, name: .tickingClock, object: nil)
+
+let notificationSubscriber = Subscribers.Assign(object: clock, keyPath: \Clock.seconds)
+
+let converter = Publishers.Map(upstream: notificationPublisher, transform: { notification -> Int in
+    return (notification.object as? Clock)?.seconds ?? 0
+})
+
+converter.subscribe(notificationSubscriber)
+
+// if cancellable is deallocated, the subscription will get cancelled
+var reference: Cancellable?
+reference = converter.sink(receiveValue: { seconds in
+    print(seconds)
+    if seconds > 4 {
+        reference?.cancel()
+//        reference = nil
+    }
+})
+
+clock.start()
+
+// RUN the clock
+
+
+let _ = Just(5) // <Int, Never>
+    .map { value -> String in
+        // operation with the incoming value here
+        // and return a string
+        return "a string \(value)"
+    }
+    .sink { receivedValue in // <String, Never>
+        // sink is the subscriber and terminates the pipeline
+        print("The end result was \(receivedValue)")
+    }
+
+// RUN Just once
+
+var pokeView  = PokeView()
+var publisher = PassthroughSubject<Int, Never>()
+var cancellable: AnyCancellable? = nil
+
+cancellable = publisher
+    .map { id -> String in
+        "https://pokeapi.co/api/v2/pokemon/\(id)"
+    }
+    .map { string -> URL in
+        URL(string: string)!
+    }
+    .tryMap { url -> Data in
+        try Data(contentsOf: url)
+    }
+    .decode(type: Pokemon.self, decoder: JSONDecoder())
+    .sink(receiveCompletion: { completion in
+        switch completion {
+        case .failure(let error):
+            fatalError(error.localizedDescription)
+        case .finished:
+            print("subscription closes")
+            break
+        }
+    }, receiveValue: { value in
+        pokeView.pokemon = value
+    })
+
+pokeView.segmentAction = { index in
+    publisher.send(index + 1)
+}
+
+PreviewWindow(pokeView.view)
+
+// RUN PassthroughSubject
+
+let timer = PassthroughSubject<Bool, Never>()
+
+let cancel = publisher.zip(timer)
+    .sink(receiveValue: { a, b in
+        print(a, b)
+    })
+
+DispatchQueue.global().asyncAfter(deadline: .now() + 5, execute: {
+    timer.send(true)
+})
+
+// RUN
+
+
+// Operators functions
+// https://heckj.github.io/swiftui-notes/#coreconcepts-operators
+
+// Practical examples for combine
+// https://github.com/AvdLee/CombineSwiftPlayground

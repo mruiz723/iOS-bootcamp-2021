f0312b0593e2edaeb78e7b07028a3fe59bde34f5
Jorge A. Benavides <jorge.benavides@wizeline.com>
2021-11-29 18:39:49 -0600

    horizontal list on swift ui

---
+++ HorizontalList.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: A UIKit based Playground for presenting user interface
+  
+import SwiftUI
+import PlaygroundSupport
+
+
+struct Poster: Identifiable {
+    var id:Int
+
+    static func arrayWith(size: Int) -> [Poster] {
+        var arr = [Poster]()
+        (0...size).forEach {
+            arr.append(Poster(id: $0))
+        }
+        return arr
+    }
+}
+
+struct HorizontalList: View {
+
+    @State var posters = Poster.arrayWith(size: 100)
+
+    var rows: [GridItem] = [
+        GridItem(.fixed(10)),
+    ]
+
+    var body: some View {
+        ScrollView {
+
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: rows, alignment: .center) {
+                    ForEach(posters) { poster in
+                        Text("\(poster.id)")
+                    }
+                }
+                .frame(width: 200, height: 200, alignment: .leading)
+            }
+            .padding()
+        }
+    }
+}
+
+// Present the view controller in the Live View window
+PlaygroundPage.current.setLiveView(HorizontalList())
+
